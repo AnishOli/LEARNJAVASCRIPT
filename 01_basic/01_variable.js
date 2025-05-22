1d7b5c8e3fe5701825161aef4560a911cdf7cf7c
AnishOli <anisholi751106@gmail.com>
2025-05-22 15:40:06 +0800

    first commit

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,18 @@
+const accountId = 142345;
+let accountEmail = "Olianish@gmail.com";
+var  accountPassword = "12443";
+  accountCity = "Bardiya";
+  let accountState;
+
+ //accountId = 232; in const we cannot change the value
+
+/* 
+prefer not to use Var because of issue in block scope and functional scope
+
+*/
+ accountEmail = "ram@gmail.com"
+ accountPassword ="1212121";
+ accountCity = "KTM"
+ console.log(accountEmail);
+ console.table([accountEmail,accountPassword,accountCity, accountState])
+ 
---
+++ 01_basic/02_Datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code   as newer  version
+
+//alert( 3 + 3 );  we are using nodejs not browser
+console.log(3 + 3);
+console.log("Anish");
+// Code readability should be high
+
+let name = "Anish";
+let age = 21;
+let isLoggedIn = true
+
+// number  =>  2 to  power  53
+// bigint
+// string => " "
+// boolean => true or false
+// null => standalone value
+// undefined  =>
+// symbol  => unique
+// object 
+
+
+console.log(typeof "Anish");
+console.log(typeof null);
+console.log(typeof undefined);
