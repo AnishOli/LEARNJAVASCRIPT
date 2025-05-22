a166dd744c82af00ee5bd2da4dd5313a3beba381
AnishOli <anisholi751106@gmail.com>
2025-05-22 16:15:40 +0800

    conversion of Datatypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = "23acc";
+console.log(typeof score);
+
+ let valueInNumber = Number(score);
+ console.log(typeof (valueInNumber));
+ console.log(valueInNumber);
+
+
+ //"33" => 33
+ //"33abc" => NaN
+ // true => 1
+ //false =>0
+
+ let isLoggedIn = "";
+ let booleanIsLoggedIn = Boolean(isLoggedIn);
+ console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// " " => false
+// "Anish" =>false
+
+let someNumber = 24;
+let someString = String(someNumber);
+console.log(someString);
+console.log(typeof someString);
