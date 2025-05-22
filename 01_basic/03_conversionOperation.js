e94e8942179c84385ba7a6c0e9fe858d444eb8dd
AnishOli <anisholi751106@gmail.com>
2025-05-22 16:56:34 +0800

    operations

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,9 +1,9 @@
 let score = "23acc";
-console.log(typeof score);
+//console.log(typeof score);
 
  let valueInNumber = Number(score);
- console.log(typeof (valueInNumber));
- console.log(valueInNumber);
+//  console.log(typeof (valueInNumber));
+//  console.log(valueInNumber);
 
 
  //"33" => 33
@@ -13,7 +13,7 @@ console.log(typeof score);
 
  let isLoggedIn = "";
  let booleanIsLoggedIn = Boolean(isLoggedIn);
- console.log(booleanIsLoggedIn);
+ //console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // " " => false
@@ -21,5 +21,39 @@ console.log(typeof score);
 
 let someNumber = 24;
 let someString = String(someNumber);
-console.log(someString);
-console.log(typeof someString);
+// console.log(someString);
+// console.log(typeof someString);
+
+
+
+//   Operations
+let value = 3
+ let negval= -value;
+//  console.log(negval);
+//  console.log(typeof negval);
+
+// console.log(2-2)
+// console.log(2+2)
+// console.log(2*2)
+// console.log(2**3)
+
+
+let str1 = "Hello";
+
+let str2 = " World";
+ let final = str1 + str2 ;
+ //console.log(final);
+
+// console.log("1" +2 +2); // the output will be in  string  because first valuse is in string so it will take all the remaining value as string too;
+ //console.log(1+2+ "3"); // here first is number so it will add and remaining 3 will be trated as string ;
+
+//  let x = 3;
+// const y = x++;
+
+// console.log(`x:${x}, y:${y}`);
+
+let x = 3;
+const y = ++x;
+
+console.log(`x:${x}, y:${y}`);
+ 
