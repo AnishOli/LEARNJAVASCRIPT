a9ecf1de07163be352573a456ece4e7c96948ddd
AnishOli <anisholi751106@gmail.com>
2025-06-02 18:14:03 +0800

    Date js

---
+++ 01_basic/06_date.js
@@ -0,0 +1,18 @@
+//  let myDate = new Date();
+//  console.log(myDate.toISOString());
+//  console.log(myDate.toJSON())
+//  console.log(myDate.toDateString());
+//  console.log(myDate.toLocaleDateString());
+//  console.log(myDate.toLocaleString())
+
+
+ //let createDate = new Date(2025, 5, 2, 5, 8)
+ //let createDate = new Date("2025-07-2")
+ let createDate = new Date("05-21-2023")
+
+ console.log(createDate.toLocaleString());
+
+
+ let mytimestamp =  Date.now();
+//  console.log(createDate.getTime());
+ console.log(Math.floor(Date.now()/1000))
